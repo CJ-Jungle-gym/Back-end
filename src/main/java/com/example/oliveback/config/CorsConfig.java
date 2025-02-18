2ece6723b48b9b7b4e2a466e1bd38332e395ec31
kyebalza <rpguswns@naver.com>
2025-02-18 19:51:52 +0900

    ｃｏｒｓ update

--- src/main/java/com/example/oliveback/config/CorsConfig.java
+++ src/main/java/com/example/oliveback/config/CorsConfig.java
@@ -1,5 +1,5 @@
 package com.example.oliveback.config;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,16 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowedOrigins}")
+    private String frontendUrl;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-                        .allowedOrigins("http://localhost:5173") // 프론트엔드 주소
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                // 모든 API에 대해 CORS 설정
+                registry.addMapping("/**") // 모든 API 경로에 대해
+                        .allowedOrigins(frontendUrl) // React 앱이 실행되는 URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
                         .allowedHeaders("*") // 모든 헤더 허용
-                        .allowCredentials(true); // 인증정보 포함 허용 (예: JWT)
+                        .allowCredentials(true); // 자격 증명 허용
             }
         };
     }
